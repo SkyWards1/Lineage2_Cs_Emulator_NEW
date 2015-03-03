5c6ba6e3b9227b1c305ed3f8220e84a01a66cc14
SkyWard <skywards1@outlook.com>
2015-03-03 23:59:15 +0300

    Начал писать логин.
    Накидал подключение к Data Base.
    Все желающие присоединяйтесь)

---
+++ L2AuthD/L2AuthD/Program.cs
@@ -0,0 +1,26 @@
+﻿using L2AuthD.connectSqlDataBase;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L2AuthD
+{
+     class Program
+    {
+
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+         [STAThread]
+         static void Main(string[] args)
+         {
+             Application.EnableVisualStyles();
+             Application.SetCompatibleTextRenderingDefault(false);
+             Application.Run(new Form1());
+         }
+    }
+}
---
+++ L2AuthD/L2AuthD/connectSqlDataBase/ConnectionDataBase.cs
@@ -0,0 +1,41 @@
+﻿using L2AuthD.configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2AuthD.connectSqlDataBase
+{
+    class ConnectionDataBase
+    {
+        private static ConnectionDataBase sqlDataBase = new ConnectionDataBase();
+
+        public static ConnectionDataBase getInstance()
+        {
+            return sqlDataBase;
+        }
+
+        protected MySqlConnectionStringBuilder connectionBuilder;
+
+        public ConnectionDataBase()
+        {
+            connectionBuilder = new MySqlConnectionStringBuilder();
+            connectionBuilder.Add("Database", Сonfiguratione.DATABASE_NAME);
+            connectionBuilder.Add("Data Source", Сonfiguratione.DATABASE_HOST);
+            connectionBuilder.Add("User Id", Сonfiguratione.DATABASE_USER_ID);
+            connectionBuilder.Add("Password", Сonfiguratione.DATABASE_PASSWORD);
+
+            Console.WriteLine("Sql подключение к Data Base установлено");
+
+        }
+
+        public MySqlConnection connection()
+        {
+            return new MySqlConnection(connectionBuilder.ConnectionString);
+        }
+
+
+    }
+}
